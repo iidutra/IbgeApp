4dcea2b9575c44c7168a5aedcf2432efb36fdaaf
pestana1984 <pestana1984@gmail.com>
2023-12-11 21:29:01 -0300

    Criado MVVM

---
+++ IbgeApp/Models/Ibge.cs
@@ -0,0 +1,9 @@
+﻿namespace IbgeApp.Models
+{
+    public class Ibge
+    {
+        public int Id { get; set; }
+        public string State { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+    }
+}
