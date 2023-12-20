b464f973e1717acafddd19b99700ac5f981ca76b
pestana1984 <pestana1984@gmail.com>
2023-12-20 11:49:41 -0300

    Commit das Views finalizadas.

--- IbgeApp/Models/Ibge.cs
+++ IbgeApp/Models/Ibge.cs
@@ -1,9 +1,16 @@
-﻿namespace IbgeApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace IbgeApp.Models
 {
     public class Ibge
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(2, ErrorMessage = "O campo não pode ser vazio!")]
         public string State { get; set; } = string.Empty;
+        [Required]
+        [MinLength(2, ErrorMessage = "O nome tem que possuir mais de 2 caracteres!")]
+        [MaxLength(40, ErrorMessage = "O nome não pode ter mais de 40 caracteres")]
         public string City { get; set; } = string.Empty;
     }
 }
